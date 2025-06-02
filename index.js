0febcc24360a554e0af204cd5acbb41bab043733
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2025-06-03 04:36:19 +0600

    integrate login session

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = 5000;
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 // middlewere
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173", // Allow Vite frontend
@@ -32,7 +35,7 @@ async function run() {
     await client.connect();
     const userCollection = client.db("authorization").collection("users");
 
-    // user signjUp api
+    // user signUp api
     app.post("/api/signup", async (req, res) => {
       const { userName, password, shopNames } = req.body;
 
@@ -79,36 +82,84 @@ async function run() {
       }
     });
 
-    // user signIn api
+    // user signIn api with generate jwt session token
     app.post("/api/signin", async (req, res) => {
       const { userName, password, remember } = req.body; // remember is Boolean value
 
       try {
-        const isExistUserName = await userCollection.findOne({ userName });
+        const user = await userCollection.findOne({ userName });
 
         // validate userName
-        if (!isExistUserName) {
+        if (!user) {
           return res.status(400).send({ message: "Invalid user name!" });
         }
 
         // validate password using bcrypt
-        const isPasswordMatch = await bcrypt.compare(
-          password,
-          isExistUserName.password
-        );
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
 
         if (!isPasswordMatch) {
           return res.status(400).send({ message: "Invalid password!" });
         }
 
+        // Generate JWT token
+        const token = jwt.sign(
+          { userId: user._id },
+          `${process.env.JWT_SECRET_KEY}`,
+          { expiresIn: remember ? "7d" : "50m" }
+        );
+
+        // set cookie with appropriate expiretion
+
+        res.cookie("authToken", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: remember ? 7 * 24 * 60 * 60 * 1000 : 30 * 60 * 1000, // set 7 days or 30 mins
+        });
+
         const result = await userCollection.findOne({
           userName,
         });
-        res.status(200).send({ success: true, result });
+        res
+          .status(200)
+          .send({ success: true, message: "Login Successfull!", result });
       } catch (error) {
         console.error("Login Error", error);
         res.status(500).send({ message: "Internal server error." });
       }
+    });
+
+    // get user info for valid user
+    app.get("/api/userinfo", async (req, res) => {
+      try {
+        const token = req.cookies.authToken;
+        if (!token) {
+          return res.status(401).send({ message: "Unauthorized Access" });
+        }
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await userCollection.findOne({
+          _id: new ObjectId(decode.userId),
+        });
+
+        if (!user) {
+          return res.status(404).send({ message: "User Not Found!" });
+        }
+
+        res.send({ success: true, user });
+      } catch (error) {
+        res.status(401).send({ message: "Invalid or expired token!" });
+      }
+    });
+
+    // user logout api
+    app.post("/api/logout", async (req, res) => {
+      res.clearCookie("authToken", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      });
+      res.send({ success: true, message: "Logged out Successfully!" });
     });
   } finally {
     // Ensures that the client will close when you finish/error
